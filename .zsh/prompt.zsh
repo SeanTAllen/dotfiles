23199a88321e5a45f84632212abf1e516014b267
Sean T Allen <sean@monkeysnatchbanana.com>
2012-03-24 12:52:15 -0400

    Changes staged but not pushed vcs prompt icon.

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -5,7 +5,7 @@ autoload -Uz vcs_info
 zstyle ':vcs_info:*' enable git hg
 zstyle ':vcs_info:(hg*|git*):*' get-revision true
 zstyle ':vcs_info:(hg*|git*):*' check-for-changes true
-zstyle ':vcs_info:(hg*|git*):*' stagedstr "!"
+zstyle ':vcs_info:(hg*|git*):*' stagedstr "✗"
 zstyle ':vcs_info:(hg*|git*):*' unstagedstr "?"
 zstyle ':vcs_info:git*' formats "%{$reset_color%}%s:(%{$fg[yellow]%}%b%{$reset_color%}) %{$fg[yellow]%}%c %u%{$reset_color%} " 
 
