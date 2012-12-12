7dcd18351a059c7990b305f9e3305ad605f7abc3
Sean T Allen <sean@monkeysnatchbanana.com>
2012-12-12 12:15:41 -0500

    Remove JAVA_HOME env that borks java working.

--- .config/fish/environment.fish
+++ .config/fish/environment.fish
@@ -4,7 +4,6 @@ set -gx PAGER less
 set -gx LC_CTYPE $LANG
 set -gx EDITOR 'vim'
 set -gx FINDBUGS_HOME /usr/local/Cellar/findbugs/2.0.1/libexec
-set -gx JAVA_HOME /usr/libexec/java_home
 set -gx GITHUB_USER SeanTAllen
 set -gx HGUSER 'Sean T. Allen <sean@monkeysnatchbanana.com>'
 set -gx CDPATH $CDPATH . $HOME/code/
