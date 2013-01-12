9a2c852900624b984b1acff3151831885c5b09a3
Sean T Allen <sean@monkeysnatchbanana.com>
2013-01-12 09:20:22 -0500

    Update ruby gems location.

--- .config/fish/path.fish
+++ .config/fish/path.fish
@@ -1,4 +1,4 @@
-set -gx RUBYGEMS_BIN /usr/local/Cellar/ruby/1.9.3-p286/bin
+set -gx RUBYGEMS_BIN /usr/local/Cellar/ruby/1.9.3-p362/bin
 set -gx HOME_BIN $HOME/bin
 
 set -gx PATH $HOME_BIN $PATH /usr/local/sbin $RUBYGEMS_BIN
