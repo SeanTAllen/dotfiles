e4bf77443a71e314edb4a9f682a02de098c34050
Sean T Allen <sean@monkeysnatchbanana.com>
2013-01-12 08:36:39 -0500

    Add /usr/local/sbin to path.

--- .config/fish/path.fish
+++ .config/fish/path.fish
@@ -1,5 +1,5 @@
 set -gx RUBYGEMS_BIN /usr/local/Cellar/ruby/1.9.3-p286/bin
 set -gx HOME_BIN $HOME/bin
 
-set -gx PATH $HOME_BIN $PATH $RUBYGEMS_BIN
+set -gx PATH $HOME_BIN $PATH /usr/local/sbin $RUBYGEMS_BIN
 
