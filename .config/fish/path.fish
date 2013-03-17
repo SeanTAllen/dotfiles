c1d9fb133b37a8324aed1c91535c26a4108b0aa5
Sean T Allen <sean@monkeysnatchbanana.com>
2013-03-17 16:06:10 -0400

    Setup path to work with rbenv.

--- .config/fish/path.fish
+++ .config/fish/path.fish
@@ -1,2 +1,2 @@
-set -gx PATH $HOME/bin /usr/local/tranquil/bin /usr/local/bin /bin /usr/bin /usr/local/sbin /sbin /usr/sbin /usr/X11R6/bin
+set -gx PATH $HOME/.rbenv/shims $HOME/bin /usr/local/tranquil/bin /usr/local/bin /bin /usr/bin /usr/local/sbin /sbin /usr/sbin /usr/X11R6/bin
 
