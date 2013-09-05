03882cb8382ae7f47da2292b4624dfe5c3497c5e
Sean T Allen <sean@monkeysnatchbanana.com>
2013-09-05 03:14:42 -0400

    Use jdk7.

--- .config/fish/environment.fish
+++ .config/fish/environment.fish
@@ -7,4 +7,5 @@ set -gx FINDBUGS_HOME /usr/local/Cellar/findbugs/2.0.1/libexec
 set -gx GITHUB_USER SeanTAllen
 set -gx HGUSER 'Sean T. Allen <sean@monkeysnatchbanana.com>'
 set -gx CDPATH $CDPATH . $HOME/code/
+set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.7.0_10.jdk/Contents/Home
 
