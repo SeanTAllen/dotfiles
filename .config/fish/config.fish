71ced41b970b8fdf58323bb859018da1b06c6e8c
Sean T Allen <sean@monkeysnatchbanana.com>
2013-10-06 11:20:23 -0400

    Remove trailing slash from item in PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -34,7 +34,7 @@ set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.7.0_10.jdk/Contents/Hom
 ### path 
 ###
 
-set -gx PATH $HOME/.rbenv/shims $HOME/bin /usr/local/share/python/ /usr/local/bin /bin /usr/bin /usr/local/sbin /sbin /usr/sbin /usr/X11R6/bin
+set -gx PATH $HOME/.rbenv/shims $HOME/bin /usr/local/share/python /usr/local/bin /bin /usr/bin /usr/local/sbin /sbin /usr/sbin /usr/X11R6/bin
 
 ###
 ### prompt
